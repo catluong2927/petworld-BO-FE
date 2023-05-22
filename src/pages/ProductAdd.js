1000251e9aa8f4e371f7833a2194b055eae20f35
PhamPhong5500 <117330186+PhamPhong5500@users.noreply.github.com>
2023-05-22 16:19:27 +0700

    no message

--- src/pages/ProductAdd.js
+++ src/pages/ProductAdd.js
@@ -9,12 +9,12 @@ export default function ProductAdd() {
 
   const [products, setProducts] = useState({});
 
-  function handleChange(event) {
-    setProducts({
-      ...products,
-      [event.target.name]: event.target.value,
-    });
-  }
+//   function handleChange(event) {
+//     setProducts({
+//       ...products,
+//       [event.target.name]: event.target.value,
+//     });
+//   }
 
   function handleSubmit() {
     axios
@@ -31,41 +31,41 @@ export default function ProductAdd() {
     <form onSubmit={handleSubmit} className="add-form">
     <h1 className="form-title">Add a new product!</h1>
     <div className="form-group">
-        <TextField label="Name" required className="input-field" />
-        <TextField label="Description" required className="input-field" />
+        <TextField name='name' label="Name" required className="input-field" />
+        <TextField name='description' label="Description" required className="input-field" />
     </div>
     <br />
     <div className="form-group">
-        <TextField label="Image" required className="input-field" />
-        <TextField label="Price" required className="input-field" />
+        <TextField name='image' label="Image" required className="input-field" />
+        <TextField name='price' label="Price" required className="input-field" />
     </div>
     <br />
     <div className="form-group">
-        <TextField label="Product Code" required className="input-field" />
-        <TextField label="Protein" className="input-field" />
+        <TextField name='productCode' label="Product Code" required className="input-field" />
+        <TextField name='protein' label="Protein" className="input-field" />
     </div>
     <br />
     <div className="form-group">
-        <TextField label="Fats" className="input-field" />
-        <TextField label="Carbohydrates" className="input-field" />
+        <TextField name='fats' label="Fats" className="input-field" />
+        <TextField name='carbohydrates' label="Carbohydrates" className="input-field" />
     </div>
     <br />
     <div className="form-group">
-        <TextField label="Minerals" className="input-field" />
-        <TextField label="Vitamins" className="input-field" />
+        <TextField name='minerals' label="Minerals" className="input-field" />
+        <TextField name='vitamins' label="Vitamins" className="input-field" />
     </div>
     <br />
     <div className="form-group">
-        <TextField label="Animal" className="input-field" />
-        <TextField label="Sale" defaultValue={0} className="input-field" />
+        <TextField name='animal' label="Animal" className="input-field" />
+        <TextField name='sale' label="Sale" defaultValue={0} className="input-field" />
     </div>
     <br />
     <div className="form-group">
-        <TextField label="Tag" className="input-field" />
-        <TextField label="URL" className="input-field" />
+        <TextField name='tag' label="Tag" className="input-field" />
+        <TextField name='url' label="URL" className="input-field" />
     </div>
     <br />
-    <Button type="submit" variant="contained" color="primary" className="submit-button">
+    <Button type="submit" variant="contained" color="primary" className="submit-button" onClick={handleSubmit}>
         Add Product
     </Button>
 </form>
