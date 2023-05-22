a9fefe97588eb3340d2feefef381440ce0b970e1
PhamPhong5500 <117330186+PhamPhong5500@users.noreply.github.com>
2023-05-22 14:14:52 +0700

    form productAdd

--- src/pages/ProductAdd.js
+++ src/pages/ProductAdd.js
@@ -1,45 +1,74 @@
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { TextField, Button } from '@mui/material';
+import axios from 'axios';
+import React, { useState, useEffect, useRef } from 'react';
+import '../css/ProductAdd.css'
 
+export default function ProductAdd() {
+    const toast = useRef(null);
+  const PRODUCT_API = `${process.env.REACT_APP_FETCH_API}/products`;
 
-export default function ProductAdd(){
-    // const PRODUCT_API = `${process.env.REACT_APP_FETCH_API}/products`;
+  const [products, setProducts] = useState({});
 
-    // const [products, setProducts] = useState({});
+  function handleChange(event) {
+    setProducts({
+      ...products,
+      [event.target.name]: event.target.value,
+    });
+  }
 
+  function handleSubmit() {
+    axios
+      .post(`${PRODUCT_API}`, products)
+      .then((res) => {
+        window.location.href = '/products';
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    // function handleChange(event){
-    //     setProducts({
-    //         ...products,
-    //         [event.target.name]: event.target.value
-    //     });
-    // }
+  return (
+    <form onSubmit={handleSubmit} className="add-form">
+    <h1 className="form-title">Add a new product!</h1>
+    <div className="form-group">
+        <TextField label="Name" required className="input-field" />
+        <TextField label="Description" required className="input-field" />
+    </div>
+    <br />
+    <div className="form-group">
+        <TextField label="Image" required className="input-field" />
+        <TextField label="Price" required className="input-field" />
+    </div>
+    <br />
+    <div className="form-group">
+        <TextField label="Product Code" required className="input-field" />
+        <TextField label="Protein" className="input-field" />
+    </div>
+    <br />
+    <div className="form-group">
+        <TextField label="Fats" className="input-field" />
+        <TextField label="Carbohydrates" className="input-field" />
+    </div>
+    <br />
+    <div className="form-group">
+        <TextField label="Minerals" className="input-field" />
+        <TextField label="Vitamins" className="input-field" />
+    </div>
+    <br />
+    <div className="form-group">
+        <TextField label="Animal" className="input-field" />
+        <TextField label="Sale" defaultValue={0} className="input-field" />
+    </div>
+    <br />
+    <div className="form-group">
+        <TextField label="Tag" className="input-field" />
+        <TextField label="URL" className="input-field" />
+    </div>
+    <br />
+    <Button type="submit" variant="contained" color="primary" className="submit-button">
+        Add Product
+    </Button>
+</form>
 
-    // function handleSubmit(){
-    //     axios
-    //     .post(`${PRODUCT_API}`, products)
-    //     .then(res => {
-    //         window.location.href = "/products"
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     });
-    // }
-    
-
-
-    return (
-            <div>
-                <h1>Add new product !</h1>
-                {/* <form>
-                    <div>
-                        <label>Name</label>
-                        <input name="name" value={products.name || ""} 
-                        onChange={handleChange} 
-                        placeholder="Enter product name"/>
-                    </div>
-                </form> */}
-            </div>
-      );
+  );
 }
